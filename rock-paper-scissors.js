833b2ca0735a55866e54fc571de4345de0978130
Mohamedsayhii <mohamedsaihii@outlook.fr>
2021-10-20 13:59:31 +0100

    third commit

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,22 +13,35 @@ const playerSelection = function playerPlay () {
 
 
 function playRound(playerSelection, computerSelection) {
+    let winner;
     console.log(`computer choice is ${computerSelection}`);
     console.log(`your choice is ${playerSelection}`);
     if (playerSelection === computerSelection) {
         console.log('EQUAL!');
     } else if (playerSelection == `rock` && computerSelection == `paper`) {
         console.log(`You lose!`);
+        winner = `Computer`;
     } else if (playerSelection == `rock` && computerSelection == `scissors`) {
         console.log('You win!');
+        winner = `You`;
     } else if (playerSelection == `paper` && computerSelection == `scissors`) {
         console.log('You lose!');
+        winner = `Computer`;
     } else if (playerSelection == `paper` && computerSelection == `rock`) {
         console.log('You win!');
+        winner = `You`;
     } else if (playerSelection == `scissors` && computerSelection == `paper`) {
         console.log('You win!');
+        winner = `You`;
     } else if (playerSelection == `scissors` && computerSelection == `rock`) {
         console.log('You lose!');
+        winner = `Computer`;
     }
 }
 
+function game() {
+    for(i = 1; i <= 5; i++) {
+        console.log(`Round ${i}`);
+        playRound(playerSelection(), computerSelection());
+    }
+}
