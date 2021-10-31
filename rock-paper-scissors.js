0e5f7f4358a2181f35bfa8c87f127b16aeb53c0e
Mohamedsayhii <mohamedsaihii@outlook.fr>
2021-10-31 13:51:39 +0100

    prettier it!

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,67 +1,66 @@
-const computerSelection = function computerPlay () {
-    const arrayRPS = [`rock`, `paper`, `scissors`];
-    const randomElement = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
-    return randomElement;
-}
+const computerSelection = function computerPlay() {
+  const arrayRPS = [`rock`, `paper`, `scissors`];
+  const randomElement = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
+  return randomElement;
+};
 
-const playerSelection = function playerPlay () {
-    let selection = prompt(`Please enter your selection`);
-    return selection.toLowerCase();
-}
+const playerSelection = function playerPlay() {
+  let selection = prompt(`Please enter your selection`);
+  return selection.toLowerCase();
+};
 
 function playRound(playerSelection, computerSelection) {
-    let winner;
+  let winner;
 
-    console.log(`Computer choice is ${computerSelection}`);
-    console.log(`Your choice is ${playerSelection}`);
-    
-    if (playerSelection === computerSelection) {
-        console.log('EQUAL!');
-    } else if (playerSelection == `rock` && computerSelection == `paper`) {
-        console.log(`You lose this round!`);
-        winner = `Computer`;
-    } else if (playerSelection == `rock` && computerSelection == `scissors`) {
-        console.log('You win this round!');
-        winner = `You`;
-    } else if (playerSelection == `paper` && computerSelection == `scissors`) {
-        console.log('You lose this round!');
-        winner = `Computer`;
-    } else if (playerSelection == `paper` && computerSelection == `rock`) {
-        console.log('You win this round!');
-        winner = `You`;
-    } else if (playerSelection == `scissors` && computerSelection == `paper`) {
-        console.log('You win this round!');
-        winner = `You`;
-    } else if (playerSelection == `scissors` && computerSelection == `rock`) {
-        console.log('You lose this round!');
-        winner = `Computer`;
-    }
-    return winner;
+  console.log(`Computer choice is ${computerSelection}`);
+  console.log(`Your choice is ${playerSelection}`);
+
+  if (playerSelection === computerSelection) {
+    console.log("EQUAL!");
+  } else if (playerSelection == `rock` && computerSelection == `paper`) {
+    console.log(`You lose this round!`);
+    winner = `Computer`;
+  } else if (playerSelection == `rock` && computerSelection == `scissors`) {
+    console.log("You win this round!");
+    winner = `You`;
+  } else if (playerSelection == `paper` && computerSelection == `scissors`) {
+    console.log("You lose this round!");
+    winner = `Computer`;
+  } else if (playerSelection == `paper` && computerSelection == `rock`) {
+    console.log("You win this round!");
+    winner = `You`;
+  } else if (playerSelection == `scissors` && computerSelection == `paper`) {
+    console.log("You win this round!");
+    winner = `You`;
+  } else if (playerSelection == `scissors` && computerSelection == `rock`) {
+    console.log("You lose this round!");
+    winner = `Computer`;
+  }
+  return winner;
 }
 
 function game() {
-    let winner;
-    let playerScore = 0;
-    let computerScore = 0;
+  let winner;
+  let playerScore = 0;
+  let computerScore = 0;
 
-    for (i = 1; i <= 5; i++) {
-        console.log(`Round ${i}`);
-        winner = playRound(playerSelection(), computerSelection());
-        if (winner == `You`) {
-            playerScore = playerScore + 1;
-        } else if (winner == `Computer`) {
-            computerScore = computerScore + 1;
-            
-        }
-        console.log(`your score is ${playerScore}`);
-        console.log(`computer score is ${computerScore}`);
+  for (i = 1; i <= 5; i++) {
+    console.log(`Round ${i}`);
+    winner = playRound(playerSelection(), computerSelection());
+    if (winner == `You`) {
+      playerScore = playerScore + 1;
+    } else if (winner == `Computer`) {
+      computerScore = computerScore + 1;
     }
+    console.log(`your score is ${playerScore}`);
+    console.log(`computer score is ${computerScore}`);
+  }
 
-    if (playerScore > computerScore) {
-        console.log(`You win the game`);
-    } else if (playerScore < computerScore) {
-        console.log(`You lose the game`);
-    } else {
-        console.log('Casualities');
-    }
+  if (playerScore > computerScore) {
+    console.log(`You win the game`);
+  } else if (playerScore < computerScore) {
+    console.log(`You lose the game`);
+  } else {
+    console.log("Casualities");
+  }
 }
