e4824d2464e44e3633a5f6363e386db0f3ca4f25
Mohamedsayhii <mohamedsaihii@outlook.fr>
2021-11-07 00:27:53 +0100

    It works, still need verification! HAMDULA

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,13 +8,25 @@ const scissorsBtn = document.querySelector("#scissors");
 const resultRound = document.querySelector(".result");
 const resultRoundStyle = resultRound.style;
 
-let playerSelection;
+const playerScoreSpan = document.querySelector("#player-score");
+const computerScoreSpan = document.querySelector("#computer-score");
+
+const playerChoice = document.querySelector("#player-choice");
+const playerChoiceStyle = playerChoice.style;
+const computerChoice = document.querySelector("#computer-choice");
+const computerChoiceStyle = computerChoice.style;
+
+const roundNumber = document.querySelector(".round-number");
 
 const computerSelection = function computerPlay() {
   const arrayRPS = [`rock`, `paper`, `scissors`];
   const randomElement = arrayRPS[Math.floor(Math.random() * arrayRPS.length)];
   return randomElement;
 };
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let i;
 
 rockBtn.addEventListener("click", (e) => {
   playerSelection = e.target.id;
@@ -32,64 +44,68 @@ scissorsBtn.addEventListener("click", (e) => {
 });
 
 function playRound(playerSelection, computerSelection) {
-  let winner;
+  computerChoice.textContent = `${computerSelection}`;
+  playerChoice.textContent = `${playerSelection}`;
 
-  console.log(`Computer choice is ${computerSelection}`);
-  console.log(`Your choice is ${playerSelection}`);
+  playerScoreSpan.textContent = playerScore;
+  computerScoreSpan.textContent = computerScore;
 
-  if (playerSelection === computerSelection) {
-    resultRound.textContent = "EQUAL";
-    resultRoundStyle.color = "blue";
-  } else if (playerSelection == `rock` && computerSelection == `paper`) {
-    resultRound.textContent = "PC wins this round!";
-    resultRoundStyle.color = "red";
-    winner = `Computer`;
-  } else if (playerSelection == `rock` && computerSelection == `scissors`) {
-    resultRound.textContent = "You win this round!";
-    resultRoundStyle.color = "green";
-    winner = `You`;
-  } else if (playerSelection == `paper` && computerSelection == `scissors`) {
-    resultRound.textContent = "PC wins this round!";
-    resultRoundStyle.color = "red";
-    winner = `Computer`;
-  } else if (playerSelection == `paper` && computerSelection == `rock`) {
-    resultRound.textContent = "You win this round!";
-    resultRoundStyle.color = "green";
-    winner = `You`;
-  } else if (playerSelection == `scissors` && computerSelection == `paper`) {
-    resultRound.textContent = "You win this round!";
-    resultRoundStyle.color = "green";
-    winner = `You`;
-  } else if (playerSelection == `scissors` && computerSelection == `rock`) {
-    resultRound.textContent = "PC wins this round!";
-    resultRoundStyle.color = "red";
-    winner = `Computer`;
+  if (playerScore < 5 && computerScore < 5) {
+    if (playerSelection === computerSelection) {
+      resultRound.textContent = "EQUAL";
+      resultRoundStyle.color = "blue";
+    } else if (playerSelection == `rock` && computerSelection == `paper`) {
+      resultRound.textContent = "PC wins this round!";
+      resultRoundStyle.color = "red";
+
+      computerScore++;
+    } else if (playerSelection == `rock` && computerSelection == `scissors`) {
+      resultRound.textContent = "You win this round!";
+      resultRoundStyle.color = "green";
+      playerScore++;
+    } else if (playerSelection == `paper` && computerSelection == `scissors`) {
+      resultRound.textContent = "PC wins this round!";
+      resultRoundStyle.color = "red";
+      computerScore++;
+    } else if (playerSelection == `paper` && computerSelection == `rock`) {
+      resultRound.textContent = "You win this round!";
+      resultRoundStyle.color = "green";
+      playerScore++;
+    } else if (playerSelection == `scissors` && computerSelection == `paper`) {
+      resultRound.textContent = "You win this round!";
+      resultRoundStyle.color = "green";
+      playerScore++;
+    } else if (playerSelection == `scissors` && computerSelection == `rock`) {
+      resultRound.textContent = "PC wins this round!";
+      resultRoundStyle.color = "red";
+      computerScore++;
+    }
+  } else {
+    if (playerScore > computerScore) {
+      resultRound.textContent = "AHCHIIIIIH, bara rawah";
+      resultRoundStyle.color = "green";
+    } else {
+      resultRound.textContent = "Te7chelk";
+      resultRoundStyle.color = "red";
+    }
   }
-  return winner;
 }
 
-// function game() {
-//   let winner;
-//   let playerScore = 0;
-//   let computerScore = 0;
-
-//   for (i = 1; i <= 5; i++) {
-//     console.log(`Round ${i}`);
-//     winner = playRound(playerSelection, computerSelection());
-//     if (winner == `You`) {
-//       playerScore = playerScore + 1;
-//     } else if (winner == `Computer`) {
-//       computerScore = computerScore + 1;
-//     }
-//     console.log(`your score is ${playerScore}`);
-//     console.log(`computer score is ${computerScore}`);
+// function game(play) {
+//   let winner = play();
+//   if (winner === `you`) {
+//     playerScore++;
+//   } else if (winner === `computer`) {
+//     computerScore++;
 //   }
+//   computerScoreSpan.textContent = `${computerScore}`;
+//   playerScoreSpan.textContent = `${playerScore}`;
 
-//   if (playerScore > computerScore) {
-//     console.log(`You win the game`);
-//   } else if (playerScore < computerScore) {
-//     console.log(`You lose the game`);
-//   } else {
-//     console.log("Casualities");
-//   }
+// if (playerScore > computerScore) {
+//   console.log(`You win the game`);
+// } else if (playerScore < computerScore) {
+//   console.log(`You lose the game`);
+// } else {
+//   console.log("Casualities");
+// }
 // }
